5cb0bdcc617e1cc27c077202567fac485ba465b2
Bastien Nocera <hadess@hadess.net>
2016-10-21 17:04:49 +0200

    qemu static user with 'F' binfmt flag obsoletes that hack
    Yay!

--- build.sh
+++ build.sh
@@ -6,23 +6,12 @@ VERSION=2.10
 APPID=`basename $FILE .json`
 
 if [ x$TARGET != x`uname -p` -a ! -z "$TARGET" ]; then
-	if [ ! -x qemu-static ]; then
-		echo "QEmu static emulator missing as 'qemu-static' executable"
-		exit 1
-	fi
-
 	ARCH_OPT="--arch=$TARGET"
-	ARCH_EMU_OPT="--arch-emulator=`pwd`/qemu-static"
 else
 	TARGET=`uname -p`
 fi
 
-# Unset the --arch-emulator if flatpak doesn't support it
-if [ ! -z "$ARCH_EMU_OPT" ] ; then
-	flatpak-builder --help | grep -q arch-emulator || ARCH_EMU_OPT=""
-fi
-
 echo ========== Building $APPID ================
 rm -rf app
-flatpak-builder $ARCH_OPT $ARCH_EMU_OPT --ccache --require-changes --repo=hello-repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
+flatpak-builder $ARCH_OPT --ccache --require-changes --repo=hello-repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
 flatpak build-bundle $ARCH_OPT hello-repo/ $APPID.$TARGET.xdgapp $APPID master

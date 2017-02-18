dbnicholson/gnu-hello-flatpak

commit 726e6b2a47e48b7b222b969bf74f4b1acf9dacbb

Add version to the bundle filename

build.sh
@@ -14,4 +14,4 @@ fi
 echo ========== Building $APPID ================
 rm -rf app
 flatpak-builder $ARCH_OPT --ccache --require-changes --repo=hello-repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
-flatpak build-bundle $ARCH_OPT hello-repo/ $APPID.$TARGET.xdgapp $APPID master
+flatpak build-bundle $ARCH_OPT hello-repo/ $APPID-$(VERSION).$TARGET.xdgapp $APPID master

commit fb85deb1a08948d4b6f3fe053aece01607ae3c87

Fix creating of .flatpak bundle
Bug introduced in 726e6b2

build.sh
@@ -14,4 +14,4 @@ fi
 echo ========== Building $APPID ================
 rm -rf app
 flatpak-builder $ARCH_OPT --ccache --require-changes --repo=hello-repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE && \
-flatpak build-bundle $ARCH_OPT hello-repo/ $APPID-$(VERSION).$TARGET.flatpak $APPID master
+flatpak build-bundle $ARCH_OPT hello-repo/ $APPID-$VERSION.$TARGET.flatpak $APPID master
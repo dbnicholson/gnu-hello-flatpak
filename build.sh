f0819ddaaaf6db5251b01c53e5726ed3513928d2
Dan Nicholson <nicholson@endlessm.com>
2017-05-23 18:29:48 -0700

    Create the app directory
    Just a hunch, but wiping the directory might be causing the cleanup to
    fail when the build stage is skipped.

--- build.sh
+++ build.sh
@@ -13,5 +13,6 @@ fi
 
 echo ========== Building $APPID ================
 rm -rf app
+mkdir app
 flatpak-builder $ARCH_OPT --repo=hello-repo --subject="${APPID} ${VERSION}" ${EXPORT_ARGS-} app $FILE
 flatpak build-bundle $ARCH_OPT hello-repo/ $APPID-$VERSION.$TARGET.flatpak $APPID master
